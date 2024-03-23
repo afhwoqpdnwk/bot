5013fd8e27c57edd4235970b058b75a389fc9c9a
afhwoqpdnwk <163976519+afhwoqpdnwk@users.noreply.github.com>
2024-03-23 16:31:51 +0800

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,16 +1,19 @@
 import telebot
 import socket
 import requests
-from dbip import DbIpCity
+from ip2geotools.databases.noncommercial import DbIpCity
 import time
 
 # Inisialisasi bot
 bot = telebot.TeleBot("7180943695:AAHdjI562H8aMRwhcbco9VJ5hzYRARcbzdM")
 
+# Inisialisasi bot
+bot = telebot.TeleBot("TOKEN_BOT_TELEGRAM")
+
 @bot.message_handler(commands=['get_ip_info'])
 def get_ip_info(message):
     user_id = message.from_user.id
-    CHAT_ID = [5066246825]  # Ganti dengan daftar ID yang diizinkan
+    CHAT_ID = [1234567890]  # Ganti dengan daftar ID yang diizinkan
 
     if user_id not in CHAT_ID:
         bot.send_message(chat_id=user_id, text="Anda tidak diizinkan menggunakan bot ini.")
@@ -35,17 +38,17 @@ def get_ip_info(message):
 
             location_response = DbIpCity.get(ip_address, api_key='free')
 
-            message = f"══════════•●•══════════\n"
-            message += f"◈ *Alamat IP :* `{ip_address}`\n"
-            message += f"◈ *Kota :* `{city}`\n"
-            message += f"◈ *Wilayah :* `{region}`\n"
-            message += f"◈ *Negara :* `{location_response.country}`\n"
-            message += f"◈ *Zona Waktu :* `{timezone}`\n"
-            message += f"◈ *ISP :* `{isp}`\n"
-            message += f"══════════•●•══════════"
-            bot.send_message(chat_id=message.chat.id, text=message, parse_mode="Markdown")
+            message_text = f"══════════•●•══════════\n"
+            message_text += f"◈ *Alamat IP :* `{ip_address}`\n"
+            message_text += f"◈ *Kota :* `{city}`\n"
+            message_text += f"◈ *Wilayah :* `{region}`\n"
+            message_text += f"◈ *Negara :* `{location_response.country}`\n"
+            message_text += f"◈ *Zona Waktu :* `{timezone}`\n"
+            message_text += f"◈ *ISP :* `{isp}`\n"
+            message_text += f"══════════•●•══════════"
+            bot.reply_to(message, message_text, parse_mode="Markdown")
         else:
-            bot.send_message(chat_id=message.chat.id, text="Gagal mendapatkan informasi IP")
+            bot.reply_to(message, "Gagal mendapatkan informasi IP")
     except IndexError:
         bot.reply_to(message, "Gunakan perintah /get_ip_info domain/ip")
     except socket.gaierror:
