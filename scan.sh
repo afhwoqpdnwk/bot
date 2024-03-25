a500d273f004508db6deaac9aebe5c718903948e
afhwoqpdnwk <163976519+afhwoqpdnwk@users.noreply.github.com>
2024-03-25 15:05:40 +0800

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TOKEN_BOT="7180943695:AAHdjI562H8aMRwhcbco9VJ5hzYRARcbzdM"
+TOKEN_BOT=$(sed -n "22s/.*TELEGRAM_TOKEN *= *['\"]\([^'\"]*\)['\"].*/\1/p" /root/bot.py) 
 CHAT_IDS=$(awk -F'[][]' '/CHAT_ID/{print $2}' /root/bot.py)
 FILE_PATH="/root/List-IP-FT.txt"
 API_URL="https://api.telegram.org/bot$TOKEN_BOT/sendDocument" 
