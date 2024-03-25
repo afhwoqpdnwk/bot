3531957330821209e0e4f12b7826f6edd983a87a
afhwoqpdnwk <163976519+afhwoqpdnwk@users.noreply.github.com>
2024-03-25 11:25:58 +0800

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-TOKEN_BOT="7180943695:AAHdjI562H8aMRwhcbco9VJ5hzYRARcbzdM" 
-CHAT_ID="5066246825"
-FILE_PATH="/root/List-IP-FT.txt" 
-API_URL="https://api.telegram.org/bot$TOKEN_BOT/sendDocument"
+TOKEN_BOT="7180943695:AAHdjI562H8aMRwhcbco9VJ5hzYRARcbzdM"
+CHAT_IDS=$(awk -F'[][]' '/CHAT_ID/{print $2}' /root/bot.py)
+FILE_PATH="/root/List-IP-FT.txt"
+API_URL="https://api.telegram.org/bot$TOKEN_BOT/sendDocument" 
 CAPTION=$(date +"%Y-%m-%d %H:%M:%S")
 rm ip.txt
-wget -O ip.txt https://ip.yha.my.id/ip 
-grep -Eo "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" ip.txt > List-IP-FT.txt
-curl -s -F chat_id=$CHAT_ID -F document=@$FILE_PATH -F caption="$CAPTION" $API_URL
+wget -O ip.txt https://ip.yha.my.id/ip
+grep -Eo "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" ip.txt >List-IP-FT.txt
+for CHAT_ID in ${CHAT_IDS//,/ }; do
+    echo "Sending document to chat ID: $CHAT_ID" RESPONSE=$(curl -s -F "chat_id=$CHAT_ID" -F "document=@$FILE_PATH" -F "caption=$CAPTION" $API_URL) echo "Response: $RESPONSE"
+done
