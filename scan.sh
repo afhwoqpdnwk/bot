85b411a0fcedd347d41c24e9bf00898fa4602068
afhwoqpdnwk <163976519+afhwoqpdnwk@users.noreply.github.com>
2024-05-04 00:36:28 +0800

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,7 +3,6 @@ TOKEN_BOT=$(awk -F'=' '/TELEGRAM_TOKEN/ {gsub(/[[:space:]]+/, "", $2); gsub(/["'
 CHAT_IDS=$(awk -F'[][]' '/CHAT_ID/{print $2}' /root/bot.py)
 FILE_PATH="/root/List-IP-FT.txt"
 API_URL="https://api.telegram.org/bot$TOKEN_BOT/sendDocument" 
-CAPTION=$(date +"%Y-%m-%d %H:%M:%S")
 rm ip.txt
 wget -O ip.txt https://ip.yha.my.id/ip
 while read -r line; do
@@ -14,6 +13,7 @@ while read -r line; do
         printf "%-15s | %s | %s\n" "$url" "$info" "$exp"
     fi
 done < ip.txt > List-IP-FT.txt
+CAPTION=$(date +"%Y-%m-%d %H:%M:%S")
 for CHAT_ID in ${CHAT_IDS//,/ }; do
     echo "Sending document to chat ID: $CHAT_ID" RESPONSE=$(curl -s -F "chat_id=$CHAT_ID" -F "document=@$FILE_PATH" -F "caption=$CAPTION" $API_URL) echo "Response: $RESPONSE"
 done
