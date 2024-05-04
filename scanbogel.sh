261b5198bfa260dbc1a735fb1cf6c0dcffb45699
afhwoqpdnwk <163976519+afhwoqpdnwk@users.noreply.github.com>
2024-05-04 12:52:29 +0800

    Create scanbogel.sh

---
+++ scanbogel.sh
@@ -0,0 +1,20 @@
+#BOGEL
+#!/bin/bash
+TOKEN_BOT=$(awk -F'=' '/TELEGRAM_TOKEN/ {gsub(/[[:space:]]+/, "", $2); gsub(/["'"'"']/,"",$2); print $2; exit}' /root/bot.py) 
+CHAT_IDS=$(awk -F'[][]' '/CHAT_ID/{print $2}' /root/bot.py)
+FILE_PATH="/root/List-IP-BOGEL.txt"
+API_URL="https://api.telegram.org/bot$TOKEN_BOT/sendDocument" 
+rm bogel.txt
+wget -O bogel.txt https://raw.githubusercontent.com/wggly/izin/main/ipvps
+while read -r line; do
+    url=$(echo "$line" | awk '{print $4}')
+    exp=$(echo "$line" | awk '{print $3}')
+    if curl -s -m 1 -I "$url" | grep -q "Switching Protocols"; then
+        info=$(curl -s "http://ip-api.com/json/$url" | jq -r '"\(.isp) | \(.city) | \(.country)"')
+        printf "%-15s | %s | %s\n" "$url" "$info" "$exp"
+    fi
+done < bogel.txt > List-IP-BOGEL.txt
+CAPTION=$(date +"%Y-%m-%d %H:%M:%S")
+for CHAT_ID in ${CHAT_IDS//,/ }; do
+    echo "Sending document to chat ID: $CHAT_ID" RESPONSE=$(curl -s -F "chat_id=$CHAT_ID" -F "document=@$FILE_PATH" -F "caption=$CAPTION" $API_URL) echo "Response: $RESPONSE"
+done
