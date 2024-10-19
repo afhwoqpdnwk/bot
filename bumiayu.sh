4084807520fedf4a278b8303b45868d753eeb5c6
afhwoqpdnwk <163976519+afhwoqpdnwk@users.noreply.github.com>
2024-10-19 21:45:23 +0800

    Create bumiayu.sh

---
+++ bumiayu.sh
@@ -0,0 +1,24 @@
+#BUMIAYU
+#!/bin/bash
+TOKEN_BOT=$(awk -F'=' '/TELEGRAM_TOKEN/ {gsub(/[[:space:]]+/, "", $2); gsub(/["'"'"']/,"",$2); print $2; exit}' /root/bot.py) 
+CHAT_IDS=$(awk -F'[][]' '/CHAT_ID/{print $2}' /root/bot.py)
+FILE_PATH="/root/List-IP-BUMIAYU.txt"
+API_URL="https://api.telegram.org/bot$TOKEN_BOT/sendDocument" 
+rm bumiayu.txt
+wget -O bumiayu.txt https://raw.githubusercontent.com/bmayu1/izin/main/ip
+wget -O - https://raw.githubusercontent.com/BmyVpn/bumiayuvpn/main/izin >> bumiayu.txt
+while read -r line; do
+    url=$(echo "$line" | awk '{print $4}')
+    exp=$(echo "$line" | awk '{print $3}')
+    if curl -s -m 1 -I "$url" | grep -q "Switching Protocols"; then
+        info=$(curl -s "http://ip-api.com/json/$url" | jq -r '"\(.isp) | \(.city) | \(.country)"')
+        printf "%-15s | %s | %s\n" "$url" "$info" "$exp"
+    fi
+done < bumiayu.txt > List-IP-BUMIAYU.txt
+CAPTION=$(date +"%Y-%m-%d %H:%M:%S")
+
+KEYBOARD='{"inline_keyboard":[[{"text":"Menu","callback_data":"cancel"}]]}'
+
+for CHAT_ID in ${CHAT_IDS//,/ }; do
+    echo "Sending document to chat ID: $CHAT_ID" RESPONSE=$(curl -s -F "chat_id=$CHAT_ID" -F "document=@$FILE_PATH" -F "caption=$CAPTION" -F "reply_markup=$KEYBOARD" $API_URL) echo "Response: $RESPONSE"
+done
